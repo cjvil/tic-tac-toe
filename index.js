f68f2221b94b7f8cd87ae2bb5964775547bb1a2f
Christie Villanueva <christiejvillanueva@gmail.com>
2017-11-13 14:59:38 -0800

    Add non-diagonal win conditions

--- index.js
+++ index.js
@@ -3,15 +3,44 @@ class Game {
     this.board = new Board();
   }
 
-  detectWin() {
+  detectWin(piece) {
+    let topRowWin = true;
+    let middleRowWin = true;
+    let bottomRowWin = true;
+
+
+    for (var i = 0; i < 3; i++) {
+      // handle column wins
+      if (this.board.topRow[i] === piece && this.board.middleRow[i] === piece && this.board.bottomRow[i]) {
+        return true;
+      }
+
+      if (this.topRow[i] !== piece) {
+        topRowWin = false;
+      }
+
+      if (this.middleRow[i] !== piece) {
+        middleRowWin = false;
+      }
+
+      if (this.bottomRow[i] !== piece) {
+        bottomRowWin = false;
+      }
+    }
   }
+
+  if (topRowWin || middleRowWin || bottomRowWin) {
+    return true;
+  }
+
+  return false;
 }
 
 class Board {
   constructor() {
-    this.topRow = [false, false, false];
-    this.middleRow = [false, false, false];
-    this.bottomRow = [false, false, false];
+    this.topRow = [null, null, null];
+    this.middleRow = [null, null, null];
+    this.bottomRow = [null, null, null];
   }
 }
 
